00ba14bbbac590e33c6cbbfe2ace1baf50d19c88
RamsaiKoushik <ram.polisetti@iiitb.ac.in>
2024-12-07 11:55:15 +0000

    allow all origins for debugging

--- flutter-frontend/healthlink/lib/APIs.dart
+++ flutter-frontend/healthlink/lib/APIs.dart
@@ -1,5 +1,5 @@
 class API {
-  static const String baseURL = 'http://192.168.58.2:30008/api/v1';
+  static const String baseURL = 'http://192.168.184.130:8003/api/v1';
   static const String userEndpoint = '/user';
   static const String patientEndpoint = '/patient';
   static const String doctorEndpoint = '/doctor';
--- spring-backend/src/main/java/com/example/demo/DemoApplication.java
+++ spring-backend/src/main/java/com/example/demo/DemoApplication.java
@@ -35,7 +35,7 @@ public class DemoApplication implements CommandLineRunner{
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://192.168.58.2:30007")
+				registry.addMapping("/**").allowedOrigins("*")
 				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
 				// .allowCredentials(true);
 			}
